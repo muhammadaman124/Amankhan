5d0adcc96b2ef6d5217c098bc260e6a4d0b9683e
Aman khan <janiaman882@gmail.com>
2026-07-13 10:36:52 -0700

    javascript use with calculation

---
+++ app.js
@@ -0,0 +1,12 @@
+let A = 4
+let B = 6
+
+console.log( A + B );     //// Addtion 
+
+console.log( A - B );     //// Subtraction 
+
+console.log( A * B );     //// Multiply
+
+console.log( A / B);      //// Divisison 
+
+console.log( A % B );     ///// Modulus
