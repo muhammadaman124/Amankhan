592bc2e2ce8dfd2d0f23c7c1ae5c2cb75c142189
Aman khan <janiaman882@gmail.com>
2026-07-15 11:24:02 -0700

    variable and string

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-///Question:01
- 
- let addThings = 20;
- let secondThing = 10;
- let addMoreThings = addThings++ + addThings-- + --addThings + secondThing - 20;
- console.log(addMoreThings);
- console.log(addThings++);
- console.log(addThings);
+let myname =" Aman "
+console.log(myname);
 
- ///Question: 02
+let age = 18
+console.log(age);
 
- let division =  20 / 2;
- let multiplication = 10 * 4;
- let addBothThings = division + multiplication + division++;
- console.log(addBothThings);
- console.log(division);
+let city = "karachi"
+console.log(city);
+
+let course = "web development"
+console.log(course);
