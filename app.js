ea7b116587f0db19cf84bdc2b0edf3d5b0a296eb
Aman khan <janiaman882@gmail.com>
2026-07-16 00:12:03 -0700

    post and pre increment and ddecrement

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-let myname =" Aman "
-console.log(myname);
 
-let age = 18
-console.log(age);
+   let a = "3"
+   let b = num++;
+   console.log(b);
+   console.log(a);
 
-let city = "karachi"
-console.log(city);
+// POST DECREMENT
+   let a = "2"
+   let b = num--;
+   console.log(b);
+   console.log(a);
 
-let course = "web development"
-console.log(course);
+// PRE INCREMENT
+   let a ="3" 
+   let b = ++num;
+   console.log(b);
+   console.log(a);
+
+// PRE DECREMENT
+   let a = "2"
+   let b = num--;
+   console.log(b);
+   console.log(a);
+
+// Chapter 07: Eliminating ambiguity
+
+   let totalCost = 1 + 3 *  4;
+   console.log(totalCost);
+
+   let totalCost =  8 + (4 * 3);
+   console.log(totalCost);
+
+   let totalCost2 =  2 * (4 + 3);
+   console.log(totalCost2);
+
+   let resultOfComputation = (2 * 4) * 4 + 2;
+   console.log(resultOfComputation);
