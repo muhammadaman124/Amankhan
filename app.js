cb281906fe63d7fe4f8839466183d2db0e276fae
Aman khan <janiaman882@gmail.com>
2026-07-17 21:07:26 -0700

    my name ,age ,and hobbies

--- app.js
+++ app.js
@@ -1,37 +1,6 @@
-
-   let a = "3"
-   let b = num++;
-   console.log(b);
-   console.log(a);
-
-// POST DECREMENT
-   let a = "2"
-   let b = num--;
-   console.log(b);
-   console.log(a);
-
-// PRE INCREMENT
-   let a ="3" 
-   let b = ++num;
-   console.log(b);
-   console.log(a);
-
-// PRE DECREMENT
-   let a = "2"
-   let b = num--;
-   console.log(b);
-   console.log(a);
-
-// Chapter 07: Eliminating ambiguity
-
-   let totalCost = 1 + 3 *  4;
-   console.log(totalCost);
-
-   let totalCost =  8 + (4 * 3);
-   console.log(totalCost);
-
-   let totalCost2 =  2 * (4 + 3);
-   console.log(totalCost2);
-
-   let resultOfComputation = (2 * 4) * 4 + 2;
-   console.log(resultOfComputation);
+let userName = prompt( "please enter your name")
+let userage  = prompt("please enter your age")
+let userhobbies  = prompt("please enter your hobbies")
+console.log("userName", userName);
+console.log("userage", userage);
+console.log("userhobbies", userhobbies);
